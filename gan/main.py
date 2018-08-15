039ace5ea1e37cd538caa529125e87a1e16cf78f
Dougal J. Sutherland <dougal@gmail.com>
2018-08-15 18:31:46 +0100

    main.py: allow reasonable args like --multi-gpu as well as terrible -multi_gpu

--- gan/main.py
+++ gan/main.py
@@ -28,97 +28,101 @@ def make_flags(parser):
 parser = argparse.ArgumentParser()
 
 
+def add_arg(name, **kwargs):
+    "Convenience to handle reasonable names for args as well as crappy ones."
+    assert name[0] == '-'
+    assert '-' not in name[1:]
+    nice_name = '--' + name[1:].replace('_', '-')
+    return parser.add_argument(name, nice_name, **kwargs)
+
 # Optimizer
-parser.add_argument('-max_iteration',               default=150000,         type=int,       help='Epoch to train [400000]')
-parser.add_argument('-beta1',                       default=0.5,            type=float,     help='Momentum term of adam [0.5]')
-parser.add_argument('-beta2',                       default=0.9,            type=float,     help='beta2 in adam [0.9]')
-parser.add_argument('-learning_rate',               default=0.0001,         type=float,     help='Learning rate [2]')
-parser.add_argument('-learning_rate_D',             default=-1,             type=float,     help='Learning rate for discriminator, if negative same as generator [-1]')
-parser.add_argument('-dsteps',                      default=5,              type=int,       help='Number of discriminator steps in a row [1]')
-parser.add_argument('-gsteps',                      default=1,              type=int,       help='Number of generator steps in a row [1]')
-parser.add_argument('-start_dsteps',                default=10,             type=int,       help='Number of discrimintor steps in a row during first 20 steps and every 100th step [1]')
+add_arg('-max_iteration',               default=150000,         type=int,       help='Epoch to train [400000]')
+add_arg('-beta1',                       default=0.5,            type=float,     help='Momentum term of adam [0.5]')
+add_arg('-beta2',                       default=0.9,            type=float,     help='beta2 in adam [0.9]')
+add_arg('-learning_rate',               default=0.0001,         type=float,     help='Learning rate [2]')
+add_arg('-learning_rate_D',             default=-1,             type=float,     help='Learning rate for discriminator, if negative same as generator [-1]')
+add_arg('-dsteps',                      default=5,              type=int,       help='Number of discriminator steps in a row [1]')
+add_arg('-gsteps',                      default=1,              type=int,       help='Number of generator steps in a row [1]')
+add_arg('-start_dsteps',                default=10,             type=int,       help='Number of discrimintor steps in a row during first 20 steps and every 100th step [1]')
 
-
-parser.add_argument('-clip_grad',                   default=True,           type=str2bool,  help='Use gradient clippint [True]')
-parser.add_argument('-batch_norm',                  default=False,          type=str2bool,  help='Use of batch norm [False]')
-
+add_arg('-clip_grad',                   default=True,           type=str2bool,  help='Use gradient clippint [True]')
+add_arg('-batch_norm',                  default=False,          type=str2bool,  help='Use of batch norm [False]')
 
 # Initalization params
-parser.add_argument('-init',                        default=0.02,           type=float,     help='Initialization value [0.02]')
+add_arg('-init',                        default=0.02,           type=float,     help='Initialization value [0.02]')
 # dimensions
-parser.add_argument('-batch_size',                  default=64,             type=int,       help='The size of batch images [1000]')
-parser.add_argument('-real_batch_size',             default=-1,            type=int,       help='The size of batch images for real samples. If -1 then same as batch_size [-1]')
-parser.add_argument('-output_size',                 default=128,            type=int,       help='The size of the output images to produce [64]')
-parser.add_argument('-c_dim',                       default=3,              type=int,       help='Dimension of image color. [3]')
-parser.add_argument('-z_dim',                       default=128,            type=int,       help='Dimension of latent noise [128]')
-parser.add_argument('-df_dim',                      default=64,             type=int,       help='Discriminator no of channels at first conv layer [64]')
-parser.add_argument('-dof_dim',                     default=1,              type=int,       help='No of discriminator output features [1]')
-parser.add_argument('-gf_dim',                      default=64,             type=int,       help='No of generator channels [64]')
+add_arg('-batch_size',                  default=64,             type=int,       help='The size of batch images [1000]')
+add_arg('-real_batch_size',             default=-1,            type=int,       help='The size of batch images for real samples. If -1 then same as batch_size [-1]')
+add_arg('-output_size',                 default=128,            type=int,       help='The size of the output images to produce [64]')
+add_arg('-c_dim',                       default=3,              type=int,       help='Dimension of image color. [3]')
+add_arg('-z_dim',                       default=128,            type=int,       help='Dimension of latent noise [128]')
+add_arg('-df_dim',                      default=64,             type=int,       help='Discriminator no of channels at first conv layer [64]')
+add_arg('-dof_dim',                     default=1,              type=int,       help='No of discriminator output features [1]')
+add_arg('-gf_dim',                      default=64,             type=int,       help='No of generator channels [64]')
 # Directories
-parser.add_argument('-dataset',                     default="cifar10",      type=str,       help='The name of the dataset [celebA, mnist, lsun, cifar10, imagenet]')
-parser.add_argument('-name',                        default="",             type=str,       help='The name of the experiment for saving purposes ')
-parser.add_argument('-checkpoint_dir',              default="checkpoint",   type=str,       help='Directory name to save the checkpoints [checkpoint_mmd]')
-parser.add_argument('-sample_dir',                  default="sample",       type=str,       help='Directory name to save the image samples [samples_mmd]')
-parser.add_argument('-log_dir',                     default="log",          type=str,       help='Directory name to save the image samples [logs_mmd]')
-parser.add_argument('-data_dir',                    default="data",         type=str,       help='Directory containing datasets [./data]')
-parser.add_argument('-out_dir',                     default="",             type=str,       help='Directory name to save the outputs of the experiment : (log, sample, checkpoints)   [./data]')
-parser.add_argument('-config_file',                 default="",             type=str,       help='path to the config file')
-
+add_arg('-dataset',                     default="cifar10",      type=str,       help='The name of the dataset [celebA, mnist, lsun, cifar10, imagenet]')
+add_arg('-name',                        default="",             type=str,       help='The name of the experiment for saving purposes ')
+add_arg('-checkpoint_dir',              default="checkpoint",   type=str,       help='Directory name to save the checkpoints [checkpoint_mmd]')
+add_arg('-sample_dir',                  default="sample",       type=str,       help='Directory name to save the image samples [samples_mmd]')
+add_arg('-log_dir',                     default="log",          type=str,       help='Directory name to save the image samples [logs_mmd]')
+add_arg('-data_dir',                    default="data",         type=str,       help='Directory containing datasets [./data]')
+add_arg('-out_dir',                     default="",             type=str,       help='Directory name to save the outputs of the experiment : (log, sample, checkpoints)   [./data]')
+add_arg('-config_file',                 default="",             type=str,       help='path to the config file')
 
 # models
-parser.add_argument('-architecture',                default="dcgan",        type=str,       help='The name of the architecture [dcgan, g-resnet5, dcgan5]')
-parser.add_argument('-kernel',                      default="",             type=str,       help="The name of the kernel ['', 'mix_rbf', 'mix_rq', 'distance', 'dot', 'mix_rq_dot']")
-parser.add_argument('-model',                       default="mmd",          type=str,       help='The model type [mmd, smmd, swgan, wgan_gp]')
+add_arg('-architecture',                default="dcgan",        type=str,       help='The name of the architecture [dcgan, g-resnet5, dcgan5]')
+add_arg('-kernel',                      default="",             type=str,       help="The name of the kernel ['', 'mix_rbf', 'mix_rq', 'distance', 'dot', 'mix_rq_dot']")
+add_arg('-model',                       default="mmd",          type=str,       help='The model type [mmd, smmd, swgan, wgan_gp]')
+
 # training options
-parser.add_argument('-is_train',                    default=True,           type=str2bool,  help='True for training, False for testing [Train]')
-parser.add_argument('-visualize',                   default=False,          type=str2bool,  help='True for visualizing, False for nothing [False]')
-parser.add_argument('-is_demo',                     default=False,          type=str2bool,  help='For testing [False]')
+add_arg('-is_train',                    default=True,           type=str2bool,  help='True for training, False for testing [Train]')
+add_arg('-visualize',                   default=False,          type=str2bool,  help='True for visualizing, False for nothing [False]')
+add_arg('-is_demo',                     default=False,          type=str2bool,  help='For testing [False]')
 
+add_arg('-log',                         default=True,           type=str2bool,  help='Wheather to write log to a file in samples directory [True]')
+add_arg('-compute_scores',              default=True,           type=str2bool,  help='Compute scores')
+add_arg('-print_pca',                   default=False,          type=str2bool,  help='Print the PCA')
+add_arg('-suffix',                      default="",             type=str,       help="Fo additional settings ['', '_tf_records']")
+add_arg('-gpu_mem',                     default=.9,             type=float,     help="GPU memory fraction limit [1.0]")
+add_arg('-no_of_samples',               default=100000,         type=int,       help="number of samples to produce")
+add_arg('-save_layer_outputs',          default=0,              type=int,       help="Wheather to save_layer_outputs. If == 2, saves outputs at exponential steps: 1, 2, 4, ..., 512 and every 1000. [0, 1, 2]")
+add_arg('-ckpt_name',                   default="",             type=str,       help=" Name of the checkpoint to load ")
 
-parser.add_argument('-log',                         default=True,           type=str2bool,  help='Wheather to write log to a file in samples directory [True]')
-parser.add_argument('-compute_scores',              default=True,           type=str2bool,  help='Compute scores')
-parser.add_argument('-print_pca',                   default=False,          type=str2bool,  help='Print the PCA')
-parser.add_argument('-suffix',                      default="",             type=str,       help="Fo additional settings ['', '_tf_records']")
-parser.add_argument('-gpu_mem',                     default=.9,             type=float,     help="GPU memory fraction limit [1.0]")
-parser.add_argument('-no_of_samples',               default=100000,         type=int,       help="number of samples to produce")
-parser.add_argument('-save_layer_outputs',          default=0,              type=int,       help="Wheather to save_layer_outputs. If == 2, saves outputs at exponential steps: 1, 2, 4, ..., 512 and every 1000. [0, 1, 2]")
-parser.add_argument('-ckpt_name',                   default="",             type=str,       help=" Name of the checkpoint to load ")
 # Decay rates
-parser.add_argument('-decay_rate',                  default=.8,             type=float,     help='Decay rate [1.0]')
-parser.add_argument('-gp_decay_rate',               default=.8,             type=float,     help='Decay rate of the gradient penalty [1.0]')
-parser.add_argument('-sc_decay_rate',               default=1.,             type=float,     help='Decay of the scaling factor')
-parser.add_argument('-restart_lr',                  default=False,          type=str2bool,  help='Wheather to use lr scheduler based on 3-sample test')
-parser.add_argument('-restart_sc',                  default=False,          type=str2bool,  help='Ensures the discriminator network is injective by adding the input to the feature')
-parser.add_argument('-MMD_lr_scheduler',            default=True,           type=str2bool,  help='Whether to use lr scheduler based on 3-sample test')
-parser.add_argument('-MMD_sdlr_past_sample',        default=10,             type=int,       help='lr scheduler: number of past iterations to keep')
-parser.add_argument('-MMD_sdlr_num_test',           default=3,              type=int,       help='lr scheduler: number of failures to decrease KID score')
-parser.add_argument('-MMD_sdlr_freq',               default=2000,           type=int,       help='lr scheduler: frequency of scoring the model')
-# discriminator penalties
-parser.add_argument('-gradient_penalty',            default=0.0,            type=float,     help='Use gradient penalty if > 0.0 [0.0]')
-parser.add_argument('-L2_discriminator_penalty',    default=0.0,            type=float,     help="Use L2 penalty on discriminator features if > 0.0 [0.0]")
-# scaled MMD
-parser.add_argument('-with_scaling',                default=False,          type=str2bool,  help='Use scaled MMD')
-parser.add_argument('-scaling_coeff',               default=10.,            type=float,     help='coeff of scaling')
-parser.add_argument('-scaling_variant',             default='grad',         type=str,       help='The variant of the scaled MMD   [value_and_grad, grad]')
-# spectral normalization
+add_arg('-decay_rate',                  default=.8,             type=float,     help='Decay rate [1.0]')
+add_arg('-gp_decay_rate',               default=.8,             type=float,     help='Decay rate of the gradient penalty [1.0]')
+add_arg('-sc_decay_rate',               default=1.,             type=float,     help='Decay of the scaling factor')
+add_arg('-restart_lr',                  default=False,          type=str2bool,  help='Wheather to use lr scheduler based on 3-sample test')
+add_arg('-restart_sc',                  default=False,          type=str2bool,  help='Ensures the discriminator network is injective by adding the input to the feature')
+add_arg('-MMD_lr_scheduler',            default=True,           type=str2bool,  help='Whether to use lr scheduler based on 3-sample test')
+add_arg('-MMD_sdlr_past_sample',        default=10,             type=int,       help='lr scheduler: number of past iterations to keep')
+add_arg('-MMD_sdlr_num_test',           default=3,              type=int,       help='lr scheduler: number of failures to decrease KID score')
+add_arg('-MMD_sdlr_freq',               default=2000,           type=int,       help='lr scheduler: frequency of scoring the model')
 
-parser.add_argument('-with_sn',                     default=False,          type=str2bool,  help='use spectral normalization')
-parser.add_argument('-with_learnable_sn_scale',     default=False,          type=str2bool,  help='train the scale of normalized weights')
+# discriminator penalties
+add_arg('-gradient_penalty',            default=0.0,            type=float,     help='Use gradient penalty if > 0.0 [0.0]')
+add_arg('-L2_discriminator_penalty',    default=0.0,            type=float,     help="Use L2 penalty on discriminator features if > 0.0 [0.0]")
+
+# scaled MMD
+add_arg('-with_scaling',                default=False,          type=str2bool,  help='Use scaled MMD')
+add_arg('-scaling_coeff',               default=10.,            type=float,     help='coeff of scaling')
+add_arg('-scaling_variant',             default='grad',         type=str,       help='The variant of the scaled MMD   [value_and_grad, grad]')
+add_arg('-use_gaussian_noise',          default=False,          type=str2bool,  help='Add N(0, 10^2) noise to images in scaling')
+
+# spectral normalization
+add_arg('-with_sn',                     default=False,          type=str2bool,  help='use spectral normalization')
+add_arg('-with_learnable_sn_scale',     default=False,          type=str2bool,  help='train the scale of normalized weights')
 
 # incomplete cholesky options for sobolevmmd
-parser.add_argument('--use-incomplete-cho',         default=True,           type=str2bool,  help="whether to use incomplete Cholesky for sobolevmmd [true]")
-parser.add_argument('--incho-eta',                  default=1e-3,           type=float,     help="stopping criterion for incomplete cholesky [%(default)s]")
-parser.add_argument('--incho-max-steps',            default=1000,           type=int,       help="iteration cap for incomplete cholesky [%(default)s]")
+add_arg('-use-incomplete-cho',          default=True,           type=str2bool,  help="whether to use incomplete Cholesky for sobolevmmd [true]")
+add_arg('-incho-eta',                   default=1e-3,           type=float,     help="stopping criterion for incomplete cholesky [%(default)s]")
+add_arg('-incho-max-steps',             default=1000,           type=int,       help="iteration cap for incomplete cholesky [%(default)s]")
 
 # multi-gpu training
-parser.add_argument('-multi_gpu',                   default=False,          type=str2bool,  help=' train accross multiple gpus in a multi-tower fashion')
-parser.add_argument('-num_gpus',                    default=1,              type=int,       help='Number of GPUs to use')
+add_arg('-multi_gpu',                   default=False,          type=str2bool,  help=' train accross multiple gpus in a multi-tower fashion')
+add_arg('-num_gpus',                    default=1,              type=int,       help='Number of GPUs to use')
 # conditional gan, only for imagenet
-parser.add_argument('-with_labels',                 default=False,          type=str2bool,  help='Conditional GAN')
-
-
-
-parser.add_argument('-use_gaussian_noise',          default=False,          type=str2bool,  help='Conditional GAN')
+add_arg('-with_labels',                 default=False,          type=str2bool,  help='Conditional GAN')
 
 
 FLAGS = make_flags(parser)
